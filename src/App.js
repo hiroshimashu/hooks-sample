f1c1789e5de0ec222e93deca0be122b37e2683f0
hiroshimashu <hiroshimashu@gmail.com>
2019-12-29 22:22:09 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import ResourceList from './ResourceList';
+
+const App = () => {
+	const [resource, setResource] = useState('posts');
+
+	return (
+		<div>
+			<div>
+				<button onClick={() => setResource('posts')}>posts</button>
+				<button onClick={() => setResource('todos')}>todos</button>
+			</div>
+			<ResourceList resource={resource} />
+		</div>
+	);
+};
+export default App;
---
+++ src/ResourceList.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const ResourceList = () => {
+	const [resources, setResources] = useState([]);
+
+	const fetchResource = async () => {
+		const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+		setResources(response.data);
+	};
+	return <div>{resources.length}</div>;
+};
+
+export default ResourceList;
